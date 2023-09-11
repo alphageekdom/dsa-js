4c2860579ea21462ff7f91da07494a452a5a7007
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-11 14:00:47 -0700

    find max number

---
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -0,0 +1,13 @@
+const findMaxNumber = (arr) => {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return max;
+};
+
+modules.export = findMaxNumber;
