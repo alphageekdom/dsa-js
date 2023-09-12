e5260df6a24e0849fbf8b8db4c8c1e632589a23f
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-11 20:51:42 -0700

    display likes updated

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -9,6 +9,8 @@ const displayLikes = (names) => {
     return `${names[0]} and ${names[1]} likes this`;
   } else if (length === 3) {
     return `${names[0]}, ${names[1]}, and ${names[2]} likes this`;
+  } else {
+    return `${names[0]}, ${names[1]}, and ${length - 2} others likes this`;
   }
 };
 
