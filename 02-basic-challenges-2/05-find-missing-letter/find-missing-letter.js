954fff2123ec5465cace13adce2821c73ba3b336
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-12 09:46:20 -0700

    alternate solution

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,13 +1,25 @@
 const findMissingLetter = (arr) => {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-  const startIndex = alphabet.indexOf(arr[0]);
+  // const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  // const startIndex = alphabet.indexOf(arr[0]);
 
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] !== alphabet[startIndex + i]) {
+  //     return alphabet[startIndex + i];
+  //   }
+  // }
+  // return '';
+
+  let start = arr[0].charCodeAt(0);
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== alphabet[startIndex + i]) {
-      return alphabet[startIndex + i];
+    let current = arr[i].charCodeAt(0);
+
+    if (current - start > 1) {
+      current = String.fromCharCode(start + 1);
     }
+
+    start = current;
   }
-  return '';
+  return start;
 };
 
 console.log(findMissingLetter(['a', 'b', 'd']));
