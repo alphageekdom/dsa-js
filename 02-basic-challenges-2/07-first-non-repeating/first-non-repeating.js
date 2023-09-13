ae4aec2293f2bbbf2b001265f3462c69674e735d
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2023-09-12 17:56:39 -0700

    alternate non repeating solution

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,13 +1,26 @@
 const firstNonRepeating = (str) => {
-  const charCount = {};
+  //   const charCount = {};
+
+  //   for (const char of str) {
+  //     charCount[char] = (charCount[char] || 0) + 1;
+  //   }
+  //   for (const char of str) {
+  //     if (charCount[char] === 1) return char;
+  //   }
+
+  //   return null;
+
+  const charCount = new Map();
 
   for (const char of str) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
-  for (const char of str) {
-    if (charCount[char] === 1) return char;
+    charCount.set(char, (charCount.get(char) || 0) + 1);
   }
 
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
   return null;
 };
 
